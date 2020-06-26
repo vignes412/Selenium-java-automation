7a8ec9f744ab93c838e48bc53ec8b6d4d4973b87
vignesh <r.vignesh412@gmail.com>
2020-06-26 23:39:45 +0530

    build error and version fixed

--- src/ExcelControl/ExcelReader.java
+++ src/ExcelControl/ExcelReader.java
@@ -15,7 +15,7 @@ public class ExcelReader {
     public  boolean isRowEmpty(Row row) {
         for (int c = row.getFirstCellNum(); c < row.getLastCellNum(); c++) {
             Cell cell = row.getCell(c);
-            if (cell != null && cell.getCellType() != CellType.BLANK)
+            if (cell != null && cell.getCellType() != CellType.BLANK.getCode())
                 return false;
         }
         return true;
